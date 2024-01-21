48259af9d818396c2f5ee86648d14773eb8e91cf
Alvaropopo <alvaromzakwana@gmail.com>
2024-01-21 13:30:21 +0000

    Update Numpy

---
+++ Numpytest.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# x1=np.random.randint(1,100,size=100)
+# print(x1)
+# print(x1[77])
+# print(x1.ndim)
+# print(x1.shape)
+# print(x1.size)
+# x1=np.random.randint(1,100,size=(10,10))
+# print(x1)
+# print(x1[3])
+# print(x1[5][6])
+# print(x1.ndim)
+# print(x1.shape)
+# print(x1.size)
+
+# x1=np.array([1,2,3])
+# print(x1)
+
+# x1=np.array([[1,2,3,4,5,6],
+#              [1,2,3,7,8,9],
+#              [1,2,3,10,11,12]])
+# print(x1)
+
+# x1=np.zeros((5,20))
+# print(x1)
+
+# x1=np.ones((5,20))
+# print(x1)
+
+# x1=np.arange(100)
+# print(x1)
+
+x1=np.arange(30,100)
+print(x1)
