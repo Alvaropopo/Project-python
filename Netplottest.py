c09a73b187bef513a5213d7912c95c898283565a
Alvaropopo <alvaromzakwana@gmail.com>
2024-02-25 20:58:06 +0000

    suku.csv
    Suku yang kuat

--- Netplottest.py
+++ Netplottest.py
@@ -3,5 +3,9 @@ import matplotlib.pyplot as plt
 
 data=pd.read_csv("suku.csv")
 print(data.columns)
-plt.bar(data["Suku"],data["JumlahPenduduk"])
+# plt.bar(data["Suku"][0:5],data["JumlahPenduduk"][0:5],color=["green","blue","yellow","red","black"])
+# plt.show()
+# plt.pie(data["JumlahPenduduk"][0:5],labels=data["Suku"][0:5],autopct='%0.1f%%')
+# plt.show()
+plt.pie(data["JumlahPenduduk"][-11:-1],labels=data["Suku"][-11:-1],autopct='%0.1f%%')
 plt.show()
